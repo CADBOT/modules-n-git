c1a1fc1cf045bba267c267f19551aae7908bee1e
Hana Fallisgaard <hfgaard@gmail.com>
2015-07-07 14:23:46 -0700

    add multiplication and division

--- calculator.js
+++ calculator.js
@@ -5,3 +5,17 @@ exports.addition = function(x, y) {
 exports.subtraction = function(x, y) {
   return x - y;
 };
+
+exports.multiplication = function(inputArray) {
+  var total = Number(inputArray[0]) * Number(inputArray[1]);
+  if (inputArray.length > 2) {
+    for (var i = 2; i < inputArray.length; i++) {
+      total *= Number(inputArray[i]);
+    }
+  }
+  return total;
+};
+
+exports.division = function(x, y) {
+  return x / y;
+};
