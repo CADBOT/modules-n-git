39a658b740a0bb841a03a414986e9cc402022f42
Dylan T. Moberg <dylanmoberg@msn.com>
2015-07-07 14:15:59 -0700

    added multiplication and division functionality

--- calculator.js
+++ calculator.js
@@ -5,3 +5,11 @@ exports.addition = function(x, y) {
 exports.subtraction = function(x, y) {
   return x - y;
 };
+
+exports.multiplication = function(x,y) {
+	return x * y;
+}
+
+exports.division = function (x,y) {
+	return x / y;
+}
--- cli.js
+++ cli.js
@@ -4,3 +4,9 @@ var a = Number(process.argv[2]);
 var b = Number(process.argv[3]);
 
 console.log(calc.addition(a,b));
+
+console.log(calc.subtraction(a,b));
+
+console.log(calc.division(a,b));
+
+console.log(calc.multipilication(a,b));
