801f633ba884a5bb9136ac01cc96def4039b7470
Colin Coltrera <c.coltrera@gmail.com>
2015-07-07 14:22:44 -0700

    Add multiplication and division methods, change CLI and previous methods to allow for an arbitrary number of inputs.

--- calculator.js
+++ calculator.js
@@ -1,7 +1,15 @@
-exports.addition = function(x, y) {
-  return x + y;
+exports.addition = function(numbers) {
+  return numbers.reduce(function(x,y) {return x + y});
 };
 
-exports.subtraction = function(x, y) {
-  return x - y;
+exports.subtraction = function(numbers) {
+  return numbers.reduce(function(x,y) {return x - y});
 };
+
+exports.multiplication = function(numbers) {
+  return numbers.reduce(function(x,y) {return x * y});
+}
+
+exports.division = function(numbers) {
+  return numbers.reduce(function(x,y) {return x / y});
+}
